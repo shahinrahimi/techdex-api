2fcad6f5a667991fc4518975a63893de00d44383
Shahin Rahimi <sh.rahimi.3d@gmail.com>
2023-05-21 23:19:57 +0300

    general backend ready

---
+++ config/allowedOrigins.js
@@ -0,0 +1,6 @@
+const allowedOrigins = [
+    // 'https://polygonal.onrender.com' , // react app
+    'http://localhost:3000',
+]
+
+module.exports = allowedOrigins
--- server.js
+++ server.js
@@ -6,12 +6,39 @@ const path =  require('path')
 const mongoose = require('mongoose')
 
 const connectDB = require('./config/dbConn')
+const corsOptions = require('./config/corsOptions')
+
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+
+const errorHandler = require('./middleware/errorHandler')
+const { logger, logEvent } = require('./middleware/logger')
 
 const PORT = process.env.PORT || 3505
 
 console.log(`Node: ${process.env.NODE_ENV}`)
 
 connectDB()
+
+// middleware
+app.use(logger)
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.use("/", require('./routes/root'))
+
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' })
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
 
 mongoose.connection.once('open', ()=>{
   console.log('Connected to MongoDB')
